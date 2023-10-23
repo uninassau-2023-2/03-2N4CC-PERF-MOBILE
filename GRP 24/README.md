c4ee0e81a81fa6d11f7e9683a259a8d93fcc6218
João Ferreira <joaoferreirape@gmail.com>
2023-10-23 20:52:35 -0300

    Avaliação do GRP 24

--- GRP 24/README.md
+++ GRP 24/README.md
@@ -1,12 +1,27 @@
 # Alunos
 
-* Guilherme Kauã - 01549420 (Scrum Master)
 * Christian felix viana de oliveira - 01561544
-* Ester Marques da Silva - 01638863
 * Emilly Vitória ribeiro dos santos- 01549416
-* William Cavalcante de Melo 01544431
-* Michael Stabele Leite da Silva Gomes - 01501519
+* Ester Marques da Silva - 01638863
+* Guilherme Kauã - 01549420 (Scrum Master)
 * Marcos de Oliveira Santos Filho - 01451172
+* Michael Stabele Leite da Silva Gomes - 01501519
+* William Cavalcante de Melo 01544431
+
+## 20223-10-23 - Apresentação
+
+* Guilherme Kauã - 01549420 (Scrum Master)
+* Código não foi enviado corretamente para o github
+* Tela de solicitação da senha
+    * Criando senha corretamente
+* Tela do atendente
+    * Implementada parcialmente
+    * Não está seguindo a regra de priorização
+* Tela do relatório
+    * Implementada parcialmente
+    * Tempo médio não implementado
+* Aplicativo no emulador
+    * Rodou normalmente
 
 ## 2023-10-16 - Apresentação
 
---
+++ GRP 24/totem/src/app/services/senhas.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SenhasService {
+
+  public lastsenha: string = '';
+  public guiche01: number = 1;
+  public guiche02: number = 2;
+  public guiche03: number = 3;
+  public guiches: string = '';
+  public senhasGeral: number = 0;
+  public senhasPrior: number = 0;
+  public senhasExame: number = 0;
+  public senhasTotal: number = 0;
+  public inputNovaSenha: string = '';
+  public senhasArray: { [key: string]: string[] } = { SG: [], SP: [], SE: [] };
+  public numeroChamado: string | null = null;
+
+  somaGeral() { this.senhasGeral++; this.senhasTotal++; }
+  somaPrior() { this.senhasPrior++; this.senhasTotal++; }
+  somaExame() { this.senhasExame++; this.senhasTotal++; }
+
+
+  novaSenha(tipoSenha: string = '') {
+    if (tipoSenha == 'SG') {
+      this.somaGeral();
+      this.inputNovaSenha =
+        new Date().getFullYear().toString().substring(2, 4) +
+        (new Date().getMonth() + 1).toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SG'].push(this.inputNovaSenha);
+    } else if (tipoSenha == 'SP') {
+      this.somaPrior();
+      this.inputNovaSenha =
+        new Date().getFullYear().toString().substring(2, 4) +
+        (new Date().getMonth() + 1).toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SP'].push(this.inputNovaSenha);
+    } else if (tipoSenha == 'SE') {
+      this.somaExame();
+      this.inputNovaSenha =
+        new Date().getFullYear().toString().substring(2, 4) +
+        (new Date().getMonth() + 1).toString().padStart(2, '0') +
+        new Date().getDate().toString().padStart(2, '0') +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SE'].push(this.inputNovaSenha);
+    }
+
+    this.numeroChamado = `A${this.senhasTotal}`;
+
+    console.log(this.senhasArray);
+  }
+
+  constructor() {
+
+  }
+}
+
