6ad04b1db32064da91b942a3346f43caf4b9c26b
João Victor Freire De Souza <joaofvictor6@gmail.com>
2023-11-11 13:54:12 -0300

    ADD novas funçoes
    funções de câmera e Pokémon aleatório

--- GRP 10/pokemonApp/src/app/tab1/tab1.page.ts
+++ GRP 10/pokemonApp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 import { PokeAPIService } from '../services/poke-api.service';
 import { ViaCepService } from '../services/via-cep.service';
@@ -12,21 +11,42 @@ export class Tab1Page {
   areaBuscarPokemon: string = '52011210';
   areaBusca: any = {
     bairro: '',
-    localidade:'',
-    logradouro:'',
-    uf:''
+    localidade: '',
+    logradouro: '',
+    uf: ''
   };
+  pokemonDetails: any = {};
+  total: number = 0;
+  totalAbilities: number = 0; // Adicione esta propriedade
+
   constructor(
     private pokeAPIService: PokeAPIService,
     private viaCepService: ViaCepService
-    ) {}
-    buscarPokemon () {
-      this.viaCepService.getViaCEPService(this.areaBuscarPokemon).subscribe((value) => {
-        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value)) ['logradouro'];
-        this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))["bairro"];
+  ) {}
+
+  buscarPokemon() {
+    this.viaCepService.getViaCEPService(this.areaBuscarPokemon)
+      .subscribe((value) => {
+        this.areaBusca.logradouro = JSON.parse(JSON.stringify(value))['logradouro'];
+        this.areaBusca.bairro = ', ' + JSON.parse(JSON.stringify(value))['bairro'];
         this.areaBusca.localidades = ' - ' + JSON.parse(JSON.stringify(value))['localidade'];
         this.areaBusca.uf = '-' + JSON.parse(JSON.stringify(value))['uf'];
       });
-      this.pokeAPIService.getPokeAPIService();
-    }
+
+    this.pokeAPIService.getPokeApiService()
+      .subscribe((pokemonDetails) => {
+        this.pokemonDetails = pokemonDetails;
+        this.calculateTotal(); // Calcule o total de height e weight
+        this.calculateTotalAbilities(); // Calcule o total de abilities
+      });
   }
+
+  calculateTotal() {
+    this.total = this.pokemonDetails.height + this.pokemonDetails.weight;
+  }
+
+  calculateTotalAbilities() {
+    this.totalAbilities = this.pokemonDetails.abilities.length;
+  }
+
+}
